ec4b531907cbcd206d6bd0edf387c3f3605e3848
EhoodGoren <ehoodgoren@gmail.com>
2021-12-21 01:27:40 +0200

    Add item button is disabled when stock is empty, and remove item button is disabled when stock is back to full

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,15 +1,25 @@
 import { connect } from 'react-redux';
 import { addItem, removeItem } from './redux/actions/cartActions';
-import { increaseStock, decreaseStock } from './redux/actions/productsActions'
+import { increaseStock, decreaseStock } from './redux/actions/productsActions';
+import products from './productsData';
 
 function Product(props) {
+    const generateAddButton = () => {
+        const disabled = props.stocks[props.item] === 0 ? true : false
+        return <button onClick={() => props.addItem(props.item, props.price)} disabled={disabled}>+</button>
+    }
+    const generateRemoveButton = () => {
+        const originalProductStock = products.find(product => product.item === props.item).stock;
+        const disabled = props.stocks[props.item] === originalProductStock ? true : false
+        return <button onClick={() => props.removeItem(props.item, props.price)} disabled={disabled}>-</button>
+    }
     return (
         <div>
             <span className="product-name">{props.item} - </span>
             <span className="product-price">{props.price}$ </span> 
             <span className="product-stock">x {props.stocks[props.item]}</span>
-            <button onClick={() => props.addItem(props.item, props.price)}>+</button>
-            <button onClick={() => props.removeItem(props.item, props.price)}>-</button>
+            {generateAddButton()}
+            {generateRemoveButton()}
         </div>
     )
 }
